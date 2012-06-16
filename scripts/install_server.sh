ffb91dd2bef8979e91cdd757fc8b18e4e9e6551c
Vinky <Vinky@live.fr>
2012-06-16 11:54:10 +0000

    improved

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -2,7 +2,7 @@
 # Automate new server installation (for iso creation)
 # src : http://code.google.com/p/ema-ghost/issues/detail?id=14
 
-required_packages="isc-dhcp-server openjdk-6-jre nfs-Kernel-server"
+required_packages="isc-dhcp-server openjdk-6-jre nfs-kernel-server"
 
 #"/etc/dhcp/dhcpd.conf
 #	faire la pool dhcp
@@ -17,13 +17,12 @@ required_packages="isc-dhcp-server openjdk-6-jre nfs-Kernel-server"
 
 optional_packages="vim screen git"
 all_packages="$required_packages $optional_packages"
-create_dirs="/mnt/sda4 /root/.ssh/ /home/emabs/.ssh/"
+create_dirs="/mnt/share /root/.ssh/ /home/emabs/.ssh/"
 
 apt-get update
 apt-get upgrade -y
 apt-get install -y $all_packages
 
-# http://code.google.com/p/ema-ghost/issues/detail?id=17
 apt-get install -y --force-yes mondo
 
 for a_dir in $create_dirs
